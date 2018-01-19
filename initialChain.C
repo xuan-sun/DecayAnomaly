e1d59a70d711d902e88bb9d0e75e21c6cb3a6c49
Xuan Sun <xuan.sun1@gmail.com>
2018-01-19 10:29:30 -0800

    Updating things. Nothing major to commit.

---
+++ initialChain.C
@@ -0,0 +1,22 @@
+{
+  TChain *chain = new TChain("pass3");
+
+  // beta runs for Octets 80 and 81.
+  chain->Add("replay_pass3_21744.root");
+  chain->Add("replay_pass3_21747.root");
+  chain->Add("replay_pass3_21749.root");
+  chain->Add("replay_pass3_21752.root");
+  chain->Add("replay_pass3_21756.root");
+  chain->Add("replay_pass3_21759.root");
+  chain->Add("replay_pass3_21761.root");
+  chain->Add("replay_pass3_21764.root");
+  chain->Add("replay_pass3_21769.root");
+  chain->Add("replay_pass3_21772.root");
+  chain->Add("replay_pass3_21774.root");
+  chain->Add("replay_pass3_21777.root");
+  chain->Add("replay_pass3_21781.root");
+  chain->Add("replay_pass3_21784.root");
+  chain->Add("replay_pass3_21786.root");
+  chain->Add("replay_pass3_21789.root");
+
+}
--- sum.C
+++ sum.C
@@ -1,9 +1,22 @@
 {
-  TChain *chain = new TChain("Total hist");
+  TFile f;
+
+  TH1D *hTotal = new TH1D("total", "Erecon Octets 80-120", 150, 0, 1500);
 
 
+  for(int i = 80; i < 122; i++)
+  {
+    if(i == 91 || i == 93 || i == 101 || i == 107 || i == 121)
+    {
+      continue;
+    }
 
+    f.Open(Form("Octet_%i_TDC_selfTimingCuts_Erecon_type1.root", i));
+    TH1D *hTemp = (TH1D*)f.Get("Total hist");
+    hTotal->Add(hTemp, 1.0);
+  }
 
+  hTotal->Draw();
 
 
 
